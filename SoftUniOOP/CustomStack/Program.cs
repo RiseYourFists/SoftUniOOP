30d830201fe3472aaa17292829f3f5ebc4f8b4f7
Emanuel Shavepeyer <daru12378@gmail.com>
2022-11-01 16:47:28 +0200

    Problem solving

---
+++ SoftUniOOP/CustomStack/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var myStack = new StackOfStrings();
+            Console.WriteLine(myStack.IsEmpty());
+            var stack = new Stack<string>();
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+            myStack.AddRange(stack);
+            Console.WriteLine(myStack.IsEmpty());
+        }
+    }
+}
---
+++ SoftUniOOP/CustomStack/StackOfStrings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty() => this.Count == 0;
+
+        public void AddRange(Stack<string> strings)
+        {
+            for (int i = 0; i < strings.Count; i++)
+            {
+                this.Push(strings.Pop());
+            }
+        }
+    }
+}
