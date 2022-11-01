cc99e2e0d75f7bf5df4912efe5988bc216de94ff
Emanuel Shavepeyer <daru12378@gmail.com>
2022-11-01 16:36:26 +0200

    Problem solving

---
+++ SoftUniOOP/RandomList/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var randomList = new RandomList() { "1","2","3","4","5","6" };
+            Console.WriteLine(randomList.RandomString());
+            Console.WriteLine(string.Join(", ", randomList));
+        }
+    }
+}
---
+++ SoftUniOOP/RandomList/RandomList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public string RandomString()
+        {
+            var rnd = new Random();
+            var index = rnd.Next(0, Count);
+            var element = this[index];
+            this.RemoveAt(index);
+            return element.ToString();
+        }
+    }
+}
