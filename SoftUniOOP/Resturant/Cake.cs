6fd354172f423b046ecd2268cd2716fe849b9cf0
Emanuel Shavepeyer <daru12378@gmail.com>
2022-11-04 20:28:28 +0200

    Problem solving

---
+++ SoftUniOOP/Resturant/Cake.cs
@@ -0,0 +1,21 @@
+﻿namespace Resturant
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Cake : Dessert
+    {
+        public static double cakeGrams = 250;
+        public static double cakeCalories = 1000;
+        public static decimal cakePrice = 5;
+
+
+        public Cake(string name) 
+            : base(name, cakePrice, cakeGrams, cakeCalories)
+        {
+            
+        }
+         
+    }
+}
---
+++ SoftUniOOP/Resturant/Coffee.cs
@@ -0,0 +1,20 @@
+﻿namespace Resturant
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Coffee : HotBeverage
+    {
+        public const double coffeeMilliliters = 50;
+        public const decimal coffeePrice = 3.50m;
+
+        public Coffee(string name, double caffeine) 
+            : base(name, coffeePrice, coffeeMilliliters)
+        {
+            Caffeine = caffeine;
+        }
+
+        public double Caffeine { get; set; }
+    }
+}
