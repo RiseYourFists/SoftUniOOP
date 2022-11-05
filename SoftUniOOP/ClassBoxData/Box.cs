2abf59e60a58cca6800bc2ce285e7b041ab2fa1f
Emanuel Shavepeyer <daru12378@gmail.com>
2022-11-05 20:35:27 +0200

    Problem solving

---
+++ SoftUniOOP/ClassBoxData/Box.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+        enum IPropertyError
+        {
+            Length,
+            Width,
+            Height,
+        }
+
+        private readonly Func<double, bool> IsLessOrEqualToZero = x => x <= 0;
+
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        public double Length
+        {
+            get => length;
+            private set
+            {
+                if (IsLessOrEqualToZero(value))
+                    SendError(IPropertyError.Length);
+                length = value;
+            }
+        }
+
+        public double Width
+        {
+            get => width;
+            private set
+            {
+                if (IsLessOrEqualToZero(value))
+                    SendError(IPropertyError.Width);
+                width = value;
+            }
+        }
+
+        public double Height 
+        { 
+            get => height;
+            private set
+            {
+                if (IsLessOrEqualToZero(value))
+                    SendError(IPropertyError.Height);
+                height = value;
+            }
+        }
+
+        private void SendError(IPropertyError type)
+            => throw new ArgumentException($"{type} cannot be zero or negative.");
+
+        public double SurfaceArea()
+            => 2 * ((length * width) + (length * height) + (width * height));
+
+        public double LiteralSurfaceArea()
+            => 2 * ((length * height) + (width * height));
+
+        public double Volume()
+            => length * width * height;
+
+    }
+}
---
+++ SoftUniOOP/ClassBoxData/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ClassBoxData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var length = double.Parse(Console.ReadLine());
+            var width = double.Parse(Console.ReadLine());
+            var height = double.Parse(Console.ReadLine());
+            try
+            {
+                var box = new Box(length, width, height);
+                
+                Console.WriteLine($"Surface Area - {box.SurfaceArea():F2}\nLateral Surface Area - {box.LiteralSurfaceArea():F2}\nVolume - {box.Volume():F2}");
+            }
+            catch (Exception ex)
+            { Console.WriteLine(ex.Message); }
+
+        }
+    }
+}
