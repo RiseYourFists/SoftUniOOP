4a4f2cc6a0ff06860618f50d47c2c13cbe8fe8c4
Emanuel Shavepeyer <daru12378@gmail.com>
2022-11-05 13:53:33 +0200

    Re-uploaded due to problems

---
+++ SoftUniOOP/Restaurant/Food.cs
@@ -0,0 +1,17 @@
+﻿namespace Resturant
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Food : Product
+    {
+        public Food(string name, decimal price, double grams) 
+            : base(name, price)
+        {
+            Grams = grams;
+        }
+
+        public double Grams { get; set; }
+    }
+}
---
+++ SoftUniOOP/Restaurant/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace Restaurant
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
