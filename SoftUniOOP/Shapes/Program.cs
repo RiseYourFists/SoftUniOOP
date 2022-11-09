2ae5685582f9c274fece40d6755cf38bd0dbf0cc
Emanuel Shavepeyer <daru12378@gmail.com>
2022-11-09 20:11:53 +0200

    Problem solving

---
+++ SoftUniOOP/Shapes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var radius = int.Parse(Console.ReadLine());
+            var circle = new Circle(radius);
+
+            var width = int.Parse(Console.ReadLine());
+            var height = int.Parse(Console.ReadLine());
+            var rectangle = new Rectangle(width, height);
+
+            circle.Draw();
+            rectangle.Draw();
+        }
+    }
+}
---
+++ SoftUniOOP/Shapes/Rectangle.cs
@@ -0,0 +1,42 @@
+﻿using Shapes.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : IDrawable
+    {
+        private int width;
+        private int height;
+
+        public Rectangle(int with, int height)
+        {
+            Width = with;
+            Height = height;
+        }
+
+        public int Width { get => width; set => width = value; }
+        public int Height { get => height; set => height = value; }
+
+        public void Draw()
+        {
+            DrawLine(width, '*', '*');
+            for (int i = 1; i < height - 1; ++i)
+            {
+                DrawLine(width, '*', ' ');
+            }
+            DrawLine(Width, '*', '*');
+        }
+
+        private void DrawLine(int width, char end, char mid)
+        {
+            Console.Write(end);
+            for (int i = 1; i < width - 1; ++i)
+            {
+                Console.Write(mid);
+            }
+            Console.WriteLine(end);
+        }
+    }
+}
