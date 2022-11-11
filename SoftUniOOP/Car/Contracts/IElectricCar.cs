a905a32fd9abe857e48465d6cdd423d89518e65b
Emanuel Shavepeyer <daru12378@gmail.com>
2022-11-11 14:59:28 +0200

    Problem solving

---
+++ SoftUniOOP/Car/Contracts/IElectricCar.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Cars.Contracts
+{
+    public interface IElectricCar
+    {
+        public int Battery { get; set; }
+    }
+}
---
+++ SoftUniOOP/Car/Seat.cs
@@ -0,0 +1,26 @@
+﻿using Cars.Contracts;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        public Seat(string model, string color)
+        {
+            Model = model;
+            Color = color;
+        }
+
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+
+        public string Start()
+            => "Engine start";
+
+        public string Stop()
+            => "Breaaak!";
+
+        public override string ToString()
+            => $"{Color} Seat {Model}\n{Start()}\n{Stop()}";
+    }
+}
