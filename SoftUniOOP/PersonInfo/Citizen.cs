4e6fda427a898d77cc54fda3643ed8b483ff02b5
Emanuel Shavepeyer <daru12378@gmail.com>
2022-11-11 15:29:17 +0200

    Problem solving

---
+++ SoftUniOOP/PersonInfo/Citizen.cs
@@ -0,0 +1,18 @@
+﻿namespace PersonInfo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Citizen : IPerson
+    {
+        public Citizen(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
