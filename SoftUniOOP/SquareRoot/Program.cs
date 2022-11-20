10cefcb55b21f9a31d090648ddf6b0c233bc5062
Emanuel Shavepeyer <daru12378@gmail.com>
2022-11-20 14:24:50 +0200

    Problem solving

---
+++ SoftUniOOP/SquareRoot/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SquareRoot
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+			var num = int.Parse(Console.ReadLine());
+			try
+			{
+				if (num < 0)
+				{
+					throw new ArgumentException("Invalid number.");
+				}
+				Console.WriteLine(Math.Sqrt(num));
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+			finally
+			{
+				Console.WriteLine("Goodbye.");
+			}
+		}
+	}
+}
