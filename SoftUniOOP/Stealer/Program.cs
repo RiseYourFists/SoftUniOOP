352b5488f20ba2beec42f2f016ddf84a4cf96b0e
Emanuel Shavepeyer <daru12378@gmail.com>
2022-11-28 16:10:45 +0200

    Problem solving

--- SoftUniOOP/Stealer/Program.cs
+++ SoftUniOOP/Stealer/Program.cs
@@ -1,12 +1,16 @@
-﻿using System;
-
-namespace Stealer
+﻿namespace Stealer
 {
+    using System;
+
     public class StartUp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var spy = new Spy();
+
+            var info = spy.StealFieldInfo("Stealer.Hacker", new string[] {"username", "password" });
+
+            Console.WriteLine(info);
         }
     }
 }
---
+++ SoftUniOOP/Stealer/Spy.cs
@@ -0,0 +1,32 @@
+﻿namespace Stealer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+
+    public class Spy
+    {
+        public string StealFieldInfo(string className, string[] fieldsToInvestigate)
+        {
+            Type typeInfo = Type.GetType(className);
+
+            List<FieldInfo> fields = typeInfo.GetFields((BindingFlags)62).ToList();
+
+            var classInstance = Activator.CreateInstance(typeInfo, new object[] { });
+
+            var output = new StringBuilder();
+
+            output.AppendLine($"Class under investigation: {className}");
+
+            foreach (FieldInfo field in fields.Where(f => fieldsToInvestigate.Contains(f.Name)))
+            {
+                output.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+            }
+
+            return output.ToString();
+
+        }
+    }
+}
