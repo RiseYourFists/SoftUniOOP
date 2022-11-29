d553872be8afcb02ad0552b1fce42f6e9792f5cc
Emanuel Shavepeyer <daru12378@gmail.com>
2022-11-29 13:51:14 +0200

    Problem solving

---
+++ SoftUniOOP/AuthorProblem/Tracker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace AuthorProblem
+{
+    public class Tracker
+    {
+        public Tracker()
+        {
+        }
+
+        public void PrintMethodsByAuthor()
+        {
+            var type = typeof(StartUp);
+
+            var methods = type.GetMethods((BindingFlags)60);
+
+            foreach (var method in methods)
+            {
+                if (method.CustomAttributes.Any(a => a.AttributeType == typeof(AuthorAttribute)))
+                {
+                    var attributes = method.GetCustomAttributes(false);
+                    foreach (AuthorAttribute attr in attributes)
+                    {
+                        Console.WriteLine($"{method.Name} is written by {attr.Name}");
+                    }
+                }
+            }
+        }
+    }
+}
