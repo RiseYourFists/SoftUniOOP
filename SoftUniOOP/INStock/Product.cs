343f3e6e2281a6167c6f8aa8743740f330cea305
Emanuel Shavepeyer <daru12378@gmail.com>
2022-12-20 19:52:36 +0200

    Unfinished

---
+++ SoftUniOOP/INStock/Product.cs
@@ -0,0 +1,22 @@
+﻿using INStock.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace INStock
+{
+    public class Product : IProduct
+    {
+        public string Label { get; private set; }
+
+        public decimal Price { get; private set; }
+
+        public int Quantity { get; private set; }
+
+        public int CompareTo(IProduct other)
+        {
+            return 0;
+        }
+    }
+}
---
+++ SoftUniOOP/INStock/ProductStock.cs
@@ -0,0 +1,64 @@
+﻿using INStock.Contracts;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace INStock
+{
+    public class ProductStock : IProductStock
+    {
+        private List<IProduct> stocks = new List<IProduct>();
+
+        public IProduct this[int index] { get => stocks[index]; set => stocks[index] = value; }
+
+        public int Count => stocks.Count;
+
+        public void Add(IProduct product)
+        {
+            stocks.Add(product);
+        }
+
+        public bool Contains(IProduct product)
+        => stocks.Contains(product);
+
+        public IProduct Find(int index)
+        {
+            if (!(index < 0 || index > stocks.Count - 1))
+            {
+                throw new IndexOutOfRangeException();
+            }
+            return stocks[index];
+        }
+
+        public IEnumerable<IProduct> FindAllByPrice(decimal price)
+            => stocks.Where(x => x.Price == price);
+
+        public IEnumerable<IProduct> FindAllByQuantity(int quantity)
+            => stocks.Where(x => x.Quantity == quantity);
+
+        public IEnumerable<IProduct> FindAllInRange(decimal lo, decimal hi)
+            => stocks.Where(x => x.Price >= lo && x.Price <= hi).OrderByDescending(x => x.Price);
+
+        public IProduct FindByLabel(string label)
+            => stocks.FirstOrDefault(x => x.Label == label);
+
+        public IProduct FindMostExpensiveProduct(int count)
+            =>
+
+        public IEnumerator<IProduct> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(IProduct product)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
