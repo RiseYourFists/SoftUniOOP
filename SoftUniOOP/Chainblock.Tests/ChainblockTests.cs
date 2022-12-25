09140521091007574845880d18a23f22a26e0284
Emanuel Shavepeyer <daru12378@gmail.com>
2022-12-25 23:47:56 +0200

    Unfinished

---
+++ SoftUniOOP/Chainblock.Tests/ChainblockTests.cs
@@ -0,0 +1,86 @@
+﻿using Chainblock.Contracts;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chainblock.Tests
+{
+    [TestFixture]
+    public class ChainblockTests
+    {
+        private IChainblock blockChain;
+        private Mock<ITransaction> fakeTransaction_One;
+        private Mock<ITransaction> fakeTransaction_Two;
+
+        [Test]
+        public void Test_Add_TryAddingTransaction()
+        {
+            blockChain.Add(fakeTransaction_One.Object);
+
+            Assert.AreEqual(1, blockChain.Count);
+        }
+
+        [Test]
+        public void Test_ContainsValue_WorksProperly()
+        {
+            blockChain.Add(fakeTransaction_One.Object);
+
+            Assert.IsTrue(blockChain.Contains(fakeTransaction_One.Object));
+        }
+        
+        [Test]
+        public void Test_ContainsID_WorksProperly()
+        {
+            blockChain.Add(fakeTransaction_One.Object);
+
+            Assert.IsTrue(blockChain.Contains(1));
+        }
+
+        [Test]
+        public void Test_ChangeTransactionStatus_ChangesItsStatus()
+        {
+            blockChain.Add(fakeTransaction_One.Object);
+            blockChain.ChangeTransactionStatus(1, TransactionStatus.Failed);
+            var bChResult = blockChain.GetById(1);
+            Assert.AreEqual(TransactionStatus.Failed, bChResult.Status);
+        }
+
+        [Test]
+        public void Test_RemoveTransactionById_WorksProperly()
+        {
+            blockChain.Add(fakeTransaction_One.Object);
+            blockChain.Add(fakeTransaction_Two.Object);
+
+            blockChain.RemoveTransactionById(2);
+
+            Assert.AreEqual(1, blockChain.Count);
+            Assert.IsTrue(blockChain.Contains(2) == false);
+        }
+
+
+
+        [SetUp]
+        public void InitialData()
+        {
+            blockChain = new ChainBlock();
+            fakeTransaction_One = new Mock<ITransaction>();
+            fakeTransaction_Two = new Mock<ITransaction>();
+
+            fakeTransaction_One.Setup(ft => ft.Id).Returns(1);
+            fakeTransaction_One.SetupSet(ft => ft.Amount = 100);
+            fakeTransaction_One.SetupSet(ft => ft.Status = TransactionStatus.Successfull);
+            fakeTransaction_One.Setup(ft => ft.From).Returns("Daniel");
+            fakeTransaction_One.Setup(ft => ft.To).Returns("Robert");
+            
+            fakeTransaction_Two.Setup(ft => ft.Id).Returns(2);
+            fakeTransaction_Two.SetupSet(ft => ft.Amount = 100);
+            fakeTransaction_Two.SetupSet(ft => ft.Status = TransactionStatus.Successfull);
+            fakeTransaction_Two.Setup(ft => ft.From).Returns("Robert");
+            fakeTransaction_Two.Setup(ft => ft.To).Returns("Daniel");
+
+            
+        }
+    }
+}
--- SoftUniOOP/Chainblock/ChainBlock.cs
+++ SoftUniOOP/Chainblock/ChainBlock.cs
@@ -2,28 +2,22 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
 
 namespace Chainblock
 {
     public class ChainBlock : IChainblock
     {
-        private Dictionary<int, ITransaction> transactions;
+        private readonly Dictionary<int, ITransaction> transactions;
 
         public ChainBlock()
         {
             transactions = new Dictionary<int, ITransaction>();
         }
 
-        public int Count => transactions.Count;
-
         public void Add(ITransaction tx)
         => transactions.Add(tx.Id, tx);
-
-        public void ChangeTransactionStatus(int id, TransactionStatus newStatus)
-        => transactions[id].Status = newStatus;
 
         public bool Contains(ITransaction tx)
             => transactions.ContainsValue(tx);
@@ -31,64 +25,129 @@ namespace Chainblock
         public bool Contains(int id)
             => transactions.ContainsKey(id);
 
-        public IEnumerable<ITransaction> GetAllInAmountRange(double lo, double hi)
-            => transactions.Values.Where(x => x.Amount >= lo && x.Amount <= hi);
+        public int Count => transactions.Count;
 
-        public IEnumerable<ITransaction> GetAllOrderedByAmountDescendingThenById()
-        => transactions.Values.OrderByDescending(x => x.Amount).ThenBy(x => x.Id);
-
-        public IEnumerable<string> GetAllReceiversWithTransactionStatus(TransactionStatus status)
-        => transactions.Values.Where(x => x.Status == status).Select(x => x.To);
-
-        public IEnumerable<string> GetAllSendersWithTransactionStatus(TransactionStatus status)
-        => transactions.Values.Where(x => x.Status == status).Select(x => x.From);
-
-        public ITransaction GetById(int id)
-            => transactions.Values.FirstOrDefault(x => x.Id == id);
-
-        public IEnumerable<ITransaction> GetByReceiverAndAmountRange(string receiver, double lo, double hi)
+        public void ChangeTransactionStatus(int id, TransactionStatus newStatus)
         {
-            throw new NotImplementedException();
-        }
+            if (!transactions.ContainsKey(id))
+                throw new ArgumentException();
 
-        public IEnumerable<ITransaction> GetByReceiverOrderedByAmountThenById(string receiver)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<ITransaction> GetBySenderAndMinimumAmountDescending(string sender, double amount)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<ITransaction> GetBySenderOrderedByAmountDescending(string sender)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<ITransaction> GetByTransactionStatus(TransactionStatus status)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<ITransaction> GetByTransactionStatusAndMaximumAmount(TransactionStatus status, double amount)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerator<ITransaction> GetEnumerator()
-        {
-            throw new NotImplementedException();
+            transactions[id].Status = newStatus;
         }
 
         public void RemoveTransactionById(int id)
         {
-            throw new NotImplementedException();
+            if (!transactions.ContainsKey(id))
+                throw new InvalidOperationException();
+
+            transactions.Remove(id);
+        }
+
+        public ITransaction GetById(int id)
+        {
+            if (!transactions.ContainsKey(id))
+                throw new InvalidOperationException();
+
+            return transactions[id];
+        }
+
+        public IEnumerable<ITransaction> GetByTransactionStatus(TransactionStatus status)
+        {
+           var result = transactions.Values.Where(tx => tx.Status == status).ToArray();
+
+            if (result.Length == 0)
+                throw new InvalidOperationException();
+
+            return result;
+        }
+
+        public IEnumerable<string> GetAllSendersWithTransactionStatus(TransactionStatus status)
+        {
+            var result = transactions.Values.Where(x => x.Status == status).Select(x => x.From).ToArray();
+
+            if (result.Length == 0)
+                throw new InvalidEnumArgumentException();
+
+            return result;
+        }
+
+        public IEnumerable<string> GetAllReceiversWithTransactionStatus(TransactionStatus status)
+        {
+            var result = transactions.Values.Where(x => x.Status == status).Select(x => x.To).ToArray();
+
+            if (result.Length == 0)
+                throw new InvalidOperationException();
+
+            return result;
+        }
+        
+
+        public IEnumerable<ITransaction> GetAllOrderedByAmountDescendingThenById()
+        => transactions.Values.OrderByDescending(x => x.Amount).ThenBy(x => x.Id).ToArray();
+
+        public IEnumerable<ITransaction> GetBySenderOrderedByAmountDescending(string sender)
+        {
+            var result = transactions.Values.Where(x => x.From == sender).OrderByDescending(x => x.Amount).ToArray();
+
+            if (result.Length == 0)
+                throw new InvalidOperationException();
+
+            return result;
+        }
+
+        public IEnumerable<ITransaction> GetByReceiverOrderedByAmountThenById(string receiver)
+        {
+            var result = transactions.Values
+                .Where(x => x.To == receiver)
+                .OrderByDescending(x => x.Amount)
+                .ThenBy(x => x.Id)
+                .ToArray();
+
+            if (result.Length == 0)
+                throw new InvalidOperationException();
+
+            return result;
+        }
+
+        public IEnumerable<ITransaction> GetByTransactionStatusAndMaximumAmount(TransactionStatus status, double amount)
+            => transactions.Values.Where(x => x.Status == status && x.Amount <= amount).ToArray();
+
+        public IEnumerable<ITransaction> GetBySenderAndMinimumAmountDescending(string sender, double amount)
+        {
+            var result = transactions.Values.Where(x => x.From == sender && x.Amount >= amount).OrderByDescending(x => x.Amount).ToArray();
+
+            if (result.Length == 0)
+                throw new InvalidOperationException();
+
+            return result;
+        }
+
+        public IEnumerable<ITransaction> GetByReceiverAndAmountRange(string receiver, double lo, double hi)
+        {
+            var result = transactions.Values
+                .Where(x => x.To == receiver &&x.Amount >= lo && x.Amount < hi)
+                .OrderByDescending(x => x.Amount)
+                .ThenBy(x => x.Id)
+                .ToArray();
+
+            if (result.Length == 0)
+                throw new InvalidOperationException();
+
+            return result;
+        }
+
+        public IEnumerable<ITransaction> GetAllInAmountRange(double lo, double hi)
+            => transactions.Values.Where(x => x.Amount > lo && x.Amount < hi).ToArray();
+            
+        public IEnumerator<ITransaction> GetEnumerator()
+        {
+            foreach (var transaction in transactions.Values)
+            {
+                yield return transaction;
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
+            => GetEnumerator();
     }
 }
