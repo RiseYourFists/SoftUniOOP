13358734e28d665b52f5a8983701666ede88f756
Emanuel Shavepeyer <daru12378@gmail.com>
2022-12-27 16:27:39 +0200

    Learning thread safe singleton pattern

---
+++ SoftUniOOP/SingletonPattern/App.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public class App
+    {
+        private static App instance;
+        private static object lockObject = new();
+
+        protected App()
+        {
+
+        }
+
+        public static App Instance 
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (lockObject)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new App();
+                            Console.WriteLine("New instance has occurred!");
+                        }
+                    }
+                }
+
+                return instance;
+            }
+            
+        }
+
+        public void Execute()
+        {
+            Console.WriteLine("Executing!");
+        }
+    }
+}
---
+++ SoftUniOOP/SingletonPattern/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Threading;
+
+namespace SingletonPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var thread = new Thread(() =>
+            {
+                for (int i = 0; i < 20; i++)
+                {
+                    App.Instance.Execute();
+                }   
+            });
+
+            var thread2 = new Thread(() =>
+            {
+                for (int i = 0; i < 20; i++)
+                {
+                    App.Instance.Execute();
+                }
+            });
+
+            thread.Start();
+            thread2.Start();
+        }
+    }
+}
