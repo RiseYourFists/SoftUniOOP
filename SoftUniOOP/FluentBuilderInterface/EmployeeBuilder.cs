c77678f1480a873d818a5709c51f7cb7c97f3959
Emanuel Shavepeyer <daru12378@gmail.com>
2022-12-27 12:55:35 +0200

    Learning fluent builder

---
+++ SoftUniOOP/FluentBuilderInterface/EmployeeBuilder.cs
@@ -0,0 +1,53 @@
+﻿namespace FluentBuilderInterface
+{
+    public class EmployeeBuilderDirector 
+        : EmployeeSalaryBuilder<EmployeeBuilderDirector>
+    {
+        public static EmployeeBuilderDirector NewEmployee => new();
+    }
+
+    public class EmployeeSalaryBuilder<T> 
+        : EmployeePositionBuilder<EmployeeSalaryBuilder<T>> where T 
+        : EmployeeSalaryBuilder<T>
+    {
+        public T SetSalary(double salary)
+        {
+            employee.Salary = salary;
+            return (T)this;
+        }
+    }
+
+    public class EmployeePositionBuilder<T> 
+        : EmployeeInfoBuilder<EmployeePositionBuilder<T>> where T 
+        : EmployeePositionBuilder<T>
+    {
+        public T SetPosition(string position)
+        {
+            employee.Position = position;
+            return (T)this;
+        }
+    }
+
+    public class EmployeeInfoBuilder<T> 
+        : EmployeeBuilder where T 
+        : EmployeeInfoBuilder<T>
+    {
+        public T SetName(string name)
+        {
+            employee.Name = name;
+            return (T)this;
+        }
+    }
+
+    public abstract class EmployeeBuilder
+    {
+        protected Employee employee;
+
+        public EmployeeBuilder()
+        {
+            employee = new Employee();
+        }
+
+        public Employee Build() => employee;
+    }
+}
---
+++ SoftUniOOP/FluentBuilderInterface/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FluentBuilderInterface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var employee =  EmployeeBuilderDirector
+                .NewEmployee
+                .SetName("Bob")
+                .SetPosition("Regular Worker")
+                .SetSalary(500)
+                .Build();
+
+            Console.WriteLine(employee);
+        }
+    }
+}
